66a222f2ba498d2c9cdae97f63b6ee42c14e9473
David Adams <david.riding.rails@gmail.com>
2020-05-11 16:30:20 -0700

    add humidity and feels-like temp

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,8 @@ class WelcomeController < ApplicationController
   	response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?zip=85004&units=imperial&appid=#{ENV['openweather_api_key']}")
     @location = response['name']
     @temp = response['main']['temp']
+    @feels_like = response['main']['feels_like']
+    @humidity = response['main']['humidity']
     @weather_icon = response['weather'][0]['icon']
     @weather_words = response['weather'][0]['description']
     @cloudiness = response['clouds']['all']
